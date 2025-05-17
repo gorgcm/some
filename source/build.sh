2cdd0c330babf820692f6f7749adb9ebb0881198
gorgcm <140067816+gorgcm@users.noreply.github.com>
2025-05-17 10:46:29 +0400

    Delete source directory

--- source/build.sh
+++
@@ -1,9 +0,0 @@
-
-#!/bin/bash
-# Check for wasm-pack
-command -v wasm-pack &>/dev/null || cargo install wasm-pack
-
-# Build and copy files
-wasm-pack build --target web
-cp pkg/*.js .
-cp pkg/*.wasm .
